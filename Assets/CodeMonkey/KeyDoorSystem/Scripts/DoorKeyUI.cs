39f11e2588248bc7619ee972cbecf67ea7918a47
Du Yifeng <a4r5t6y@yeah.net>
2024-10-13 18:21:02 +0800

    Added door key system
    Door key system had been added and successfully running

---
+++ Assets/CodeMonkey/KeyDoorSystem/Scripts/DoorKeyUI.cs
@@ -0,0 +1,50 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeMonkey.KeyDoorSystemCM
+{
+
+    public class DoorKeyUI : MonoBehaviour
+    {
+        [Tooltip("The Prefab for a single UI Key")]
+        public Transform keyUISinglePrefab;
+        [Tooltip("The Key Holder that will be displayed")]
+        public DoorKeyHolder doorKeyHolder;
+
+        void Start()
+        {
+            if (doorKeyHolder == null)
+            {
+                Debug.LogError("You need to set the DoorKeyHolder field on the DoorKeyUI! Drag the Player Game Object onto it.");
+                return;
+            }
+            // Ensure doorKeyHolder is assigned before subscribing to events
+            doorKeyHolder.OnDoorKeyAdded += DoorKeyHolder_OnDoorKeyAdded;
+            doorKeyHolder.OnDoorKeyUsed += DoorKeyHolder_OnDoorKeyUsed;
+        }
+
+        private void DoorKeyHolder_OnDoorKeyAdded(object sender, System.EventArgs e)
+        {
+            // Handle the event when a key is added
+        }
+
+        private void DoorKeyHolder_OnDoorKeyUsed(object sender, System.EventArgs e)
+        {
+            // Handle the event when a key is used
+        }
+    }
+}
